48a676d02e5667347b2d199b881026a2eb5438ca
Vyacheslav Shimarulin <shimarulin@gmail.com>
2021-04-12 21:48:10 +0300

    feat: theme and format for disk select

--- src/setup/questions/disk.rs
+++ src/setup/questions/disk.rs
@@ -1,16 +1,40 @@
 use crate::setup::facts::disks;
 use dialoguer::{theme::ColorfulTheme, Select};
+use dialoguer::console::style;
 
 pub fn select_disk(block_device_list: &Vec<disks::BlockDevice>) {
+    let theme = ColorfulTheme {
+        success_suffix: style("".to_string()).for_stderr().black().bright(),
+        ..ColorfulTheme::default()
+    };
     let selections = block_device_list
         .iter()
         .map(|block_device| {
-            format!("{path:<20} test", path=block_device.path.to_string())
+            format!(
+                "{name:<9} {model:<20} {size:<8} {serial:<18}",
+                name = block_device.name.to_string(),
+                model = block_device.model.to_string(),
+                size = block_device.size.to_string(),
+                serial = block_device.serial.to_string()
+            )
         })
         .collect::<Vec<_>>();
 
-    let selection = Select::with_theme(&ColorfulTheme::default())
-        .with_prompt("Pick your flavor")
+    let prompt_table_header = style(format!(
+                "{name:<9} {model:<20} {size:<8} {serial:<18}",
+                name = "Name",
+                model = "Model",
+                size = "Size",
+                serial = "Serial"
+            )).yellow();
+
+    let prompt = format!(
+                "Select disk to install\n  {}\n",
+                prompt_table_header
+            );
+
+    let selection = Select::with_theme(&theme)
+        .with_prompt(prompt)
         .default(0)
         .items(&selections[..])
         .interact()
