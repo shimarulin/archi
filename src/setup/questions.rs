b1967319f0b881650119a36b71a38a8a58a6737c
Vyacheslav Shimarulin <shimarulin@gmail.com>
2021-04-14 01:35:00 +0300

    feat: confirm installation

--- src/setup/questions.rs
+++ src/setup/questions.rs
@@ -2,15 +2,22 @@ use crate::setup::facts::{Facts, disks};
 
 mod disk;
 mod user;
+mod confirm;
 
 pub struct Answers {
     pub disk: disks::BlockDevice,
     pub user: user::User,
+    pub confirm: bool,
 }
 
 pub fn ask_questions(facts_: &Facts) -> Answers {
+    let disk_ = disk::select_disk(&facts_.disks);
+    let user_ = user::setup_user();
+    let confirm_ = confirm::ask_confirm(&disk_);
+
     Answers {
-        disk: disk::select_disk(&facts_.disks),
-        user: user::setup_user(),
+        disk: disk_,
+        user: user_,
+        confirm: confirm_,
     }
 }
---
+++ src/setup/questions/confirm.rs
@@ -0,0 +1,29 @@
+use crate::setup::facts::disks;
+use crate::setup::questions_theme::get_questions_theme;
+use dialoguer::console::style;
+use dialoguer::{Confirm};
+
+pub fn ask_confirm(block_device: &disks::BlockDevice) -> bool {
+    let disk_info = format!(
+        "({model} {size}, serial: {serial})",
+        model = block_device.model,
+        size = block_device.size,
+        serial = block_device.serial,
+    );
+
+    let warning_note = format!(
+        "  {note_start} {path}\n  {disk_info}\n  {note_danger} {note_end}",
+        note_start = style("Please note that all data on the").yellow(),
+        path = style(&block_device.path).red(),
+        disk_info = style(disk_info).yellow(),
+        note_danger = style("will be erased").red(),
+        note_end = style("prior to installation.").yellow(),
+    );
+
+    println!("{}", warning_note);
+
+    Confirm::with_theme(&get_questions_theme())
+        .with_prompt("Do you confirm the installation?")
+        .interact()
+        .unwrap()
+}
