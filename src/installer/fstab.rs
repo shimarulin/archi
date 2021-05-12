32e63cf51a17f3d72830970e020e680ac94d7900
Vyacheslav Shimarulin <shimarulin@gmail.com>
2021-05-13 00:13:45 +0300

    refactor: generate fstab

--- src/installer/fstab.rs
+++ src/installer/fstab.rs
@@ -1,25 +1,11 @@
-use std::fs::OpenOptions;
-use std::io::Write;
 use std::process::Command;
+use crate::utils::file;
 
 pub fn generate() {
-    let mut fstab_file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        // .append(true)
-        .open("/mnt/etc/fstab")
-        .ok()
-        .expect("Couldn't open /mnt/etc/fstab file.");
-
     let output = Command::new("genfstab")
         .args(&["-U", "-p", "/mnt"])
         .output()
         .expect("failed to execute genfstab");
 
-    fstab_file
-        .write_all(&output.stdout)
-        .ok()
-        .expect("Couldn't write /mnt/etc/fstab file.");
-
-    println!("fstab created");
+    file::create("/mnt/etc/hostname", &*String::from_utf8(output.stdout).unwrap());
 }
