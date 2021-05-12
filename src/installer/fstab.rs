9f51b97be09cc59ec92c9f96a8a6ca67baa06142
Vyacheslav Shimarulin <shimarulin@gmail.com>
2021-05-13 02:14:44 +0300

    fix: file path

--- src/installer/fstab.rs
+++ src/installer/fstab.rs
@@ -7,5 +7,5 @@ pub fn generate() {
         .output()
         .expect("failed to execute genfstab");
 
-    file::create("/mnt/etc/hostname", &*String::from_utf8(output.stdout).unwrap());
+    file::create("/mnt/etc/fstab", &*String::from_utf8(output.stdout).unwrap());
 }
