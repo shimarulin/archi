0a7f44c287a6b63ff3759d70a26fd59412e3fea6
Vyacheslav Shimarulin <shimarulin@gmail.com>
2021-05-13 01:18:59 +0300

    refactor: swap utils

--- src/installer/swap.rs
+++ src/installer/swap.rs
@@ -1,19 +1,19 @@
-use std::process::{Command};
+use crate::utils::cmd;
 
 pub fn init(disk_partition_path: &str) {
-    Command::new("mkswap")
-        .arg(&disk_partition_path)
-        .output()
-        .expect("failed to execute mkswap");
+    cmd::exec(
+        "mkswap",
+        &[disk_partition_path],
+    );
 
     println!("swap on {} initialized", disk_partition_path);
 }
 
 pub fn on(disk_partition_path: &str) {
-    Command::new("swapon")
-        .arg(&disk_partition_path)
-        .output()
-        .expect("failed to execute swapon");
+    cmd::exec(
+        "swapon",
+        &[disk_partition_path],
+    );
 
     println!("swap on {} enabled", disk_partition_path);
 }
