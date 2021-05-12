59a1cfa5dffb6212453755dcb79f210d3a51e37c
Vyacheslav Shimarulin <shimarulin@gmail.com>
2021-05-13 01:10:49 +0300

    refactor: bootloader

--- src/installer/grub.rs
+++ src/installer/grub.rs
@@ -1,29 +1,24 @@
-use std::process::{Command, Stdio};
-// use crate::utils::file;
+use crate::utils::cmd;
 
 fn grub_mbr_install(disk_path: &str) {
-    Command::new("arch-chroot")
-        .arg("/mnt")
-        .args(&[
+    cmd::exec(
+        "arch-chroot",
+        &[
+            "/mnt",
             "grub-install",
             "--target=i386-pc",
             "--recheck",
             "--boot-directory=/boot",
-            // "--force",
             &disk_path,
-        ])
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .output()
-        .expect("ERR");
-
-    println!("GRUB2 BIOS installed");
+        ],
+    );
 }
 
 fn grub_efi_install(disk_path: &str) {
-    Command::new("arch-chroot")
-        .arg("/mnt")
-        .args(&[
+    cmd::exec(
+        "arch-chroot",
+        &[
+            "/mnt",
             "grub-install",
             "--target=x86_64-efi",
             "--recheck",
@@ -31,47 +26,15 @@ fn grub_efi_install(disk_path: &str) {
             "--boot-directory=/boot",
             "--efi-directory=/boot/efi",
             &disk_path,
-        ])
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .output()
-        .expect("ERR");
+        ],
+    );
 }
 
-// fn create_grub_cfg() {
-//     file::create("/mnt/boot/grub/grub.cfg", ". $prefix/config.cfg");
-// }
-
-// fn create_grub_config_cfg() {
-//     let content = "set default=0
-// set timeout=5
-// menuentry \"Arch Linux\" {
-//   probe --set=UUID --fs-uuid $root
-//   linux /@/boot/vmlinuz-linux root=UUID=$UUID ro rootflags=subvol=@
-//   initrd /@/boot/initramfs-linux.img
-// }";
-//     file::create("/mnt/boot/grub/config.cfg", content);
-// }
-
-// fn protect_grub_cfg() {
-//     Command::new("chattr")
-//         .args(&["+i", "/mnt/boot/grub/grub.cfg"])
-//         .stdout(Stdio::inherit())
-//         .stderr(Stdio::inherit())
-//         .output()
-//         .expect("ERR");
-//
-//     println!("grub.cfg protected");
-// }
-
-// fn add_grub_cfg_to_pacman_ignore() {
-//     file::replace_string("/mnt/etc/pacman.conf", "#NoUpgrade   =", "NoUpgrade   = boot/grub/grub.cfg");
-// }
-
 fn create_efi_option(disk_path: &str) {
-    Command::new("arch-chroot")
-        .arg("/mnt")
-        .args(&[
+    cmd::exec(
+        "arch-chroot",
+        &[
+            "/mnt",
             "efibootmgr",
             "-c",
             "-d",
@@ -82,27 +45,17 @@ fn create_efi_option(disk_path: &str) {
             "Arch Linux",
             "-l",
             "\\EFI\\BOOT\\BOOTX64.EFI",
-        ])
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .output()
-        .expect("ERR");
+        ],
+    );
 
     println!("UEFI boot option created");
 }
 
 fn grub_mkconfig() {
-    Command::new("arch-chroot")
-        .arg("/mnt")
-        .args(&[
-            "grub-mkconfig",
-            "-o",
-            "/boot/grub/grub.cfg",
-        ])
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .output()
-        .expect("ERR");
+    cmd::exec(
+        "arch-chroot",
+        &["/mnt", "grub-mkconfig", "-o", "/boot/grub/grub.cfg"],
+    );
 }
 
 pub fn install(disk_path: &str, firmware: &str) {
@@ -110,11 +63,6 @@ pub fn install(disk_path: &str, firmware: &str) {
     grub_efi_install(&disk_path);
 
     grub_mkconfig();
-
-    // create_grub_cfg();
-    // create_grub_config_cfg();
-    // protect_grub_cfg();
-    // add_grub_cfg_to_pacman_ignore();
 
     if firmware == "UEFI" {
         create_efi_option(&disk_path);
